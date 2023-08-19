aaf92a6eae65c8ab64ed65d4d0ce1c99a64a67ed
Muhamad Oka Augusta <oka.august@gmail.com>
2023-08-19 20:11:12 +0700

    add footer and title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,42 @@
 	import '@fontsource/poppins/800.css';
 </script>
 
+<svelte:head>
+	<title>Age Calculator | okreate</title>
+</svelte:head>
+
 <main>
 	<slot />
+	<footer>
+		<p>
+			<a
+				href="https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q"
+				target="_blank">Frontend Mentor Age Calculator App</a
+			>
+			solution by <a href="okreate.me" target="_blank">okreate</a>
+		</p>
+	</footer>
 </main>
 
 <style>
 	main {
+		position: relative;
 		height: 100%;
 		background-color: hsl(0, 0%, 94%);
 		display: flex;
 		align-items: center;
 		justify-content: center;
 		padding-inline: 1rem;
+	}
+
+	footer {
+		position: absolute;
+		bottom: 1rem;
+		text-align: center;
+		padding-inline: 1rem;
+	}
+
+	a {
+		color: hsl(259, 100%, 65%);
 	}
 </style>
